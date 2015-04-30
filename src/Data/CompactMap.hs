b75eaa86526376dfc6f935ec59c880a9a26adbd2
bartavelle <bartavelle@gmail.com>
2015-04-30 15:44:18 +0200

    No brain left

--- src/Data/CompactMap.hs
+++ src/Data/CompactMap.hs
@@ -3,11 +3,13 @@ module Data.CompactMap
     , fromList
     , toVector
     , lookup
+    , getLE
     ) where
 
 import qualified Data.Vector as V
 import GHC.Exts (sortWith)
 import Data.Foldable
+import Control.Applicative
 import Prelude hiding (lookup)
 
 data CompactMap k v = CompactMap (V.Vector v) (v -> k)
@@ -30,18 +32,29 @@ toVector :: CompactMap k v -> V.Vector v
 toVector (CompactMap v _) = v
 
 lookup :: Ord k => k -> CompactMap k v -> Maybe v
-lookup k (CompactMap lst f) = go 0 (sz - 1)
+lookup k cm@(CompactMap _ f) = getLE k cm >>= \r -> if f r == k
+                                                        then Just r
+                                                        else Nothing
+
+getLE :: Ord k => k -> CompactMap k v -> Maybe v
+getLE k (CompactMap lst f) = go 0 (sz - 1)
     where
         sz = V.length lst
         go l h
             | m < l     = Nothing
             | m > h     = Nothing
             | k' == k   = Just x
-            | l == h    = Nothing
-            | k' > k    = go l (m - 1)
+            | l >= h    = checkLower <|> Just x
+            | k < k'    = checkLower <|> go l (m - 1)
             | otherwise = go (m + 1) h
             where
+                checkLower
+                    | m < 1 = Nothing
+                    | k > k_1 = Just x_1
+                    | otherwise = Nothing
                 m = (l + h) `div` 2
                 x = lst V.! m
                 k' = f x
+                x_1 = lst V.! (m - 1)
+                k_1 = f x_1
 
