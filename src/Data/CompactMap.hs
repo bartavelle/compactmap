d5808c8e533a5c4d0d5bb1bfe204392317b707d3
bartavelle <bartavelle@gmail.com>
2015-09-22 10:14:20 +0200

    Add getIndex function

--- src/Data/CompactMap.hs
+++ src/Data/CompactMap.hs
@@ -4,6 +4,7 @@ module Data.CompactMap
     , toVector
     , lookup
     , getLE
+    , getIndex
     ) where
 
 import qualified Data.Vector as V
@@ -32,25 +33,25 @@ toVector :: CompactMap k v -> V.Vector v
 toVector (CompactMap v _) = v
 
 lookup :: Ord k => k -> CompactMap k v -> Maybe v
-lookup k cm@(CompactMap _ f) = getLE k cm >>= \r -> if f r == k
-                                                        then Just r
-                                                        else Nothing
+lookup k cm@(CompactMap _ f) = getLE k cm >>= \(_,r) -> if f r == k
+                                                            then Just r
+                                                            else Nothing
 
-getLE :: Ord k => k -> CompactMap k v -> Maybe v
+getLE :: Ord k => k -> CompactMap k v -> Maybe (Int, v)
 getLE k (CompactMap lst f) = go 0 (sz - 1)
     where
         sz = V.length lst
         go l h
             | m < l     = Nothing
             | m > h     = Nothing
-            | k' == k   = Just x
-            | l >= h    = checkLower <|> Just x
+            | k' == k   = Just (m, x)
+            | l >= h    = checkLower <|> Just (m, x)
             | k < k'    = checkLower <|> go l (m - 1)
             | otherwise = go (m + 1) h
             where
                 checkLower
                     | m < 1 = Nothing
-                    | k > k_1 = Just x_1
+                    | k > k_1 = Just (m - 1, x_1)
                     | otherwise = Nothing
                 m = (l + h) `div` 2
                 x = lst V.! m
@@ -58,3 +59,5 @@ getLE k (CompactMap lst f) = go 0 (sz - 1)
                 x_1 = lst V.! (m - 1)
                 k_1 = f x_1
 
+getIndex :: Int -> CompactMap k v -> Maybe v
+getIndex i (CompactMap lst _) = lst V.!? i
