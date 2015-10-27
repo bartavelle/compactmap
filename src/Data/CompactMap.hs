f17bd9ba53c9400e5f7837b6cc516704df33f4b9
bartavelle <bartavelle@gmail.com>
2015-10-27 11:30:27 +0100

    Reuse the 'generic' code.

--- src/Data/CompactMap.hs
+++ src/Data/CompactMap.hs
@@ -7,13 +7,12 @@ module Data.CompactMap
     , getIndex
     ) where
 
+import qualified Data.CompactMap.Generic as CM
 import qualified Data.Vector as V
-import GHC.Exts (sortWith)
 import Data.Foldable as F
-import Control.Applicative
 import Prelude hiding (lookup)
 
-data CompactMap k v = CompactMap (V.Vector v) (v -> k)
+newtype CompactMap k v = CompactMap { getCM :: CM.CompactMap V.Vector k v }
 
 instance Show v => Show (CompactMap k v) where
     show = show . V.toList . getMap
@@ -23,41 +22,21 @@ instance Foldable (CompactMap k) where
     foldMap f = F.foldMap f . getMap
 
 getMap :: CompactMap k v -> V.Vector v
-getMap (CompactMap lst _) = lst
+getMap = CM.toVector . getCM
 {-# INLINE getMap #-}
 
 fromList :: Ord k => [v] -> (v -> k) -> CompactMap k v
-fromList lst f = CompactMap (V.fromList $ sortWith f lst) f
+fromList lst f = CompactMap (CM.fromList lst f)
 
 toVector :: CompactMap k v -> V.Vector v
-toVector (CompactMap v _) = v
+toVector = getMap
+{-# INLINE toVector #-}
 
 lookup :: Ord k => k -> CompactMap k v -> Maybe v
-lookup k cm@(CompactMap _ f) = getLE k cm >>= \(_,r) -> if f r == k
-                                                            then Just r
-                                                            else Nothing
+lookup k (CompactMap cm) = CM.lookup k cm
 
 getLE :: Ord k => k -> CompactMap k v -> Maybe (Int, v)
-getLE k (CompactMap lst f) = go 0 (sz - 1)
-    where
-        sz = V.length lst
-        go l h
-            | m < l     = Nothing
-            | m > h     = Nothing
-            | k' == k   = Just (m, x)
-            | l >= h    = checkLower <|> Just (m, x)
-            | k < k'    = checkLower <|> go l (m - 1)
-            | otherwise = go (m + 1) h
-            where
-                checkLower
-                    | m < 1 = Nothing
-                    | k > k_1 = Just (m - 1, x_1)
-                    | otherwise = Nothing
-                m = (l + h) `div` 2
-                x = lst V.! m
-                k' = f x
-                x_1 = lst V.! (m - 1)
-                k_1 = f x_1
+getLE k (CompactMap cm) = CM.getLE k cm
 
 getIndex :: Int -> CompactMap k v -> Maybe v
-getIndex i (CompactMap lst _) = lst V.!? i
+getIndex i (CompactMap cm) = CM.getIndex i cm
